1bc698641f164ffb6dbc7cf1a2e6228c9dea705c
Katan Patel <katanpatel86@gmail.com>
2019-03-27 21:17:05 +0000

    update to index.html

--- app.js
+++ app.js
@@ -1,10 +1,59 @@
 console.log("connected");
 
+//DELETE ALERT ON BUTTON CLICK
+$(document).ready(function(){
+  $(".alert-button").click(function(){
+    $(".alert").remove();
+  });
+});
+
 // TRAFFIC CHART - LINE CHART
 var ctx = document.getElementById('myLineChart').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(177, 150, 196)',
+            borderColor: 'rgb(148, 123, 166)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+// DAILY TRAFFIC CHART - BAR CHART
+var ctx2 = document.getElementById('myBarChart').getContext('2d');
+var chart2 = new Chart(ctx2, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+// DAILY TRAFFIC CHART - BAR CHART
+var ctx3 = document.getElementById('myPieChart').getContext('2d');
+var chart3 = new Chart(ctx3, {
+    // The type of chart we want to create
+    type: 'doughnut',
 
     // The data for our dataset
     data: {
